e8892d9f328bf23f14a4e3be59a4f7354f1a9e19
canesche <michael.canesche@gmail.com>
2021-07-10 18:08:17 -0300

    update

--- gem5/execute.sh
+++ gem5/execute.sh
@@ -2,7 +2,7 @@
 echo "chmod +x '/content/gdrive/My Drive/gem5'/build/$1/gem5.opt"
 echo "'/content/gdrive/My Drive/gem5'/build/$1/gem5.opt $2"
 
-chmod +x '/content/gdrive/My Drive/gem5'/build/$1/gem5.opt
-'/content/gdrive/My Drive/gem5'/build/$1/gem5.opt $2
+#chmod +x '/content/gdrive/My Drive/gem5'/build/$1/gem5.opt
+#'/content/gdrive/My Drive/gem5'/build/$1/gem5.opt $2
 
 
